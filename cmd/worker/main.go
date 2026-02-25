b0ced53bc7377346f8f855dfe90b825286f129f0
Saminda Panditha <zaminda@gmail.com>
2026-02-25 21:57:45 +1100

    add leader election support for Kubernetes deployments in worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/nats-io/nats.go"
 	"github.com/zaminda/mesh-runner/internal"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/leaderelection"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 )
 
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
 	// Detect if running in Kubernetes
 	inKubernetes := os.Getenv("KUBERNETES_SERVICE_HOST") != ""
 	if inKubernetes {
@@ -37,6 +47,10 @@ func main() {
 
 	log.Println("worker started, waiting for jobs...")
 
+	if inKubernetes {
+		go runWithLeaderElection(ctx)
+	}
+
 	// Wait for SIGTERM or SIGINT
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
@@ -44,4 +58,57 @@ func main() {
 
 	log.Println("shutting down worker...")
 	log.Println("worker stopped")
+}
+
+func runWithLeaderElection(ctx context.Context) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
+	namespace := os.Getenv("POD_NAMESPACE")
+	if namespace == "" {
+		namespace = "default"
+	}
+
+	podName := os.Getenv("POD_NAME")
+	if podName == "" {
+		podName = "mesh-runner-worker-" + os.Getenv("HOSTNAME")
+	}
+
+	lock := &resourcelock.LeaseLock{
+		LeaseMeta: metav1.ObjectMeta{
+			Name:      "mesh-runner-worker-leader",
+			Namespace: namespace,
+		},
+		Client: clientset.CoordinationV1(),
+		LockConfig: resourcelock.ResourceLockConfig{
+			Identity: podName,
+		},
+	}
+
+	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		Lock:          lock,
+		LeaseDuration: 15 * time.Second,
+		RenewDeadline: 10 * time.Second,
+		RetryPeriod:   2 * time.Second,
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: func(ctx context.Context) {
+				log.Println("I AM THE LEADER")
+			},
+			OnStoppedLeading: func() {
+				log.Println("I AM NO LONGER THE LEADER")
+			},
+			OnNewLeader: func(identity string) {
+				if identity != podName {
+					log.Printf("New leader elected: %s\n", identity)
+				}
+			},
+		},
+	})
 }
--- go.mod
+++ go.mod
@@ -9,5 +9,8 @@ require (
 	github.com/nats-io/nkeys v0.4.11 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
 	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	k8s.io/api v0.35.1 // indirect
+	k8s.io/apimachinery v0.35.1 // indirect
+	k8s.io/client-go v0.35.1 // indirect
 )
